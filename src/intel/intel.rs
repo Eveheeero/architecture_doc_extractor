7b9bd6172a262837a4719b9ef49dcd02b80154ce
Eveheeero <xhve00000@naver.com>
2023-09-02 11:43:16 +0000

    +

--- src/intel/intel.rs
+++ src/intel/intel.rs
@@ -1,5 +1,7 @@
+use regex::Regex;
+
 use crate::print_pages;
-use std::collections::HashMap;
+use std::{cell::OnceCell, collections::HashMap, sync::OnceLock};
 
 pub fn main() {
     let data = extract_text();
@@ -116,4 +118,45 @@ struct Instruction {
     operation: String,
     flag_affected: String,
     exceptions: HashMap<String, Vec<String>>,
+}
+
+enum Category {
+    Summary,
+    OpcodeDescription,
+    InstructionOperandEncoding,
+    Description,
+    Operation,
+    FlagsAffected,
+    Exceptions,
+    None,
+    NotInstruction,
+}
+
+fn parse_category(data: impl AsRef<str>) -> Category {
+    static SUMMARY: OnceLock<Regex> = OnceLock::new();
+    static OPCODE_DESCRIPTION: OnceLock<Regex> = OnceLock::new();
+    static INSTRUCTION_OPERAND_ENCODING: OnceLock<Regex> = OnceLock::new();
+    static OPERATION: OnceLock<Regex> = OnceLock::new();
+    static FLAGAFFECTED: OnceLock<Regex> = OnceLock::new();
+    static EXCEPTIONS: OnceLock<Regex> = OnceLock::new();
+    let summary = SUMMARY.get_or_init(|| Regex::new("^[^a-z]*-.+$").unwrap());
+    // Opcode로 시작해서 여러 줄 거쳐서 Description으로 끝나는 경우
+    let opcode_description =
+        OPCODE_DESCRIPTION.get_or_init(|| Regex::new("^Opcode.*Description$").unwrap());
+    let instruction_operand_encoding = INSTRUCTION_OPERAND_ENCODING
+        .get_or_init(|| Regex::new("^Op/En.*Operand 1Operand 2Operand 3Operand 4$").unwrap());
+    let operation = OPERATION.get_or_init(|| Regex::new("^Operation$").unwrap());
+    let flag_effected = FLAGAFFECTED.get_or_init(|| Regex::new("^Flags Affected$").unwrap());
+    let exceptions = EXCEPTIONS.get_or_init(|| Regex::new("^.* Mode Exceptions$").unwrap());
+
+    let data = data.as_ref();
+    match () {
+        () if summary.is_match(data) => Category::Summary,
+        () if opcode_description.is_match(data) => Category::OpcodeDescription,
+        () if instruction_operand_encoding.is_match(data) => Category::InstructionOperandEncoding,
+        () if operation.is_match(data) => Category::Operation,
+        () if flag_effected.is_match(data) => Category::FlagsAffected,
+        () if exceptions.is_match(data) => Category::Exceptions,
+        _ => Category::None,
+    }
 }
