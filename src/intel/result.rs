3012f53fecf13b460b95fb30ba3516d2b5c19cbc
Eveheeero <xhve00000@naver.com>
2023-10-07 13:15:15 +0000

    +

--- src/intel/result.rs
+++ src/intel/result.rs
@@ -21,7 +21,51 @@ pub(super) struct Instruction {
 }
 
 impl Instruction {
+    /// Instruction to result string
     pub(super) fn into_string(self) -> Vec<String> {
+        let mut result = Vec::new();
+
+        // 제목
+        result.push(format!("# {title}", title = self.title));
+
+        // 요약
+        result.push("".to_owned());
+        result.push(format!("{summary}", summary = self.summary));
+
+        // 인스트럭션 별 설명
+
+        // 문서 하이퍼링크
+
+        // 설명
+        let mut description = self.get_description();
+        if !description.is_empty() {
+            result.push("".to_owned());
+            result.append(&mut description);
+        }
+        drop(description);
+
+        // 호환성
+
+        // 옵코드
+
+        // Flags affected
+        if !self.flag_affected.is_empty() {
+            result.push("".to_owned());
+            result.push("## Flags affected".to_owned());
+            result.push(format!(
+                "- {flag_affected}",
+                flag_affected = self.flag_affected
+            ));
+        }
+
+        // 예외
+        // C/C++ 코드
+        // 명령
+
+        result
+    }
+
+    fn get_description(&self) -> Vec<String> {
         todo!()
     }
 }
