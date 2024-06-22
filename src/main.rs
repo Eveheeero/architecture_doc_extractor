17ddf83843135956f7c0d5869b3ff0d2deb3549f
Eveheeero <xhve00000@naver.com>
2024-06-22 21:22:29 +0900

    +

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use simplelog::{CombinedLogger, Config, LevelFilter, SimpleLogger, WriteLogger};
 use std::fs::File;
 
 fn main() {
-    let log_level = LevelFilter::Trace;
+    let log_level = LevelFilter::Debug;
     let log_config = Config::default();
     CombinedLogger::init(vec![
         SimpleLogger::new(log_level, log_config.clone()),
